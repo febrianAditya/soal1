0c9dcc6afceb0a31eb4e10a58591c07691ee2d20
FebrianAditya <febrian.aditya@student.upi.edu>
2024-10-14 01:18:41 +0700

    add soal

---
+++ soal.js
@@ -0,0 +1,104 @@
+/**
+ * Menentukan bilangan ganjil dan genap
+ * 
+ * Buatlah program untuk menentukan input bilangan apakah ganjil atau genap. Gunakan if-else dan function yang mengembalikan hasil "ganjil"/"genap"
+ */
+
+function checkOddEven() {
+
+}
+
+console.log(checkOddEven(5)); // ganjil
+console.log(checkOddEven(2)); // genap
+console.log(checkOddEven("enam")); // invalid input
+
+
+
+/**
+ * Menentukan usia dewasa
+ * 
+ * Buat sebuah program yang meminta usia seseorang, lalu gunakan if-else untuk menampilkan apakah seseorang tersebut sudah dewasa (>=18 tahun) atau belum. Gunakan if-else dan function yang mengembalikan string
+ */
+
+function checkMature() {
+
+}
+
+console.log(checkMature(18)); // Dewasa
+console.log(checkMature(17)); // Belum dewasa
+console.log(checkMature("enam")); // invalid input
+console.log(checkMature(-1)); // invalid input
+
+
+
+/**
+ * Menentukan score student
+ * 
+ * Buat program yang mengubah nilai angka ke dalam nilai huruf (A, B, C, D, E) berdasarkan rentang berikut:
+ * 90 - 100: A
+ * 80 - 89: B
+ * 70 - 79: C
+ * 60 - 69: D
+ * < 60: E
+ * Gunakan if-else dan function yang mengembalikan string
+ */
+
+function scoreReport() {
+
+}
+
+console.log(scoreReport(90)); // A
+console.log(scoreReport(89)); // B
+console.log(scoreReport(75)); // C
+console.log(scoreReport(59)); // E
+console.log(scoreReport(101)); // invalid input
+console.log(scoreReport(-1)); // invalid input
+console.log(scoreReport("sembilan puluh")); // invalid input
+
+
+/**
+ * Menampilkan Bilangan Ganjil dari 1 sampai 20
+ * 
+ * Buat program yang menampilkan semua bilangan ganjil dari 1 sampai 20 menggunakan for loop
+ */
+
+function loopCheckOddEven() {
+
+}
+
+loopCheckOddEven(5)
+// 1 => Bilangan Ganjil
+// 2 => Bilangan Genap
+// 3 => Bilangan Ganjil
+// 4 => Bilangan Genap
+// 5 => Bilangan Ganjil
+loopCheckOddEven("lima") // invalid input
+
+
+/**
+ * Menghitung Total Jumlah Angka
+ * 
+ * Buat program yang menjumlahkan semua angka dari 1 sampai dengan 100 menggunakan loop. Gunakan function yang mengirimkan value hasil penjumlahan tersebut
+ */
+
+function totalValue() {
+
+}
+
+console.log(totalValue(100)); // 5050
+console.log(totalValue("empat")); // invalid input
+
+
+/**
+ * Menghitung huruf vokal
+ * 
+ * Buat program yang menerima sebuah string, lalu hitung berapa banyak huruf vokal (a, e, i, o, u) di dalam string tersebut. Gunakan function untuk mengembalikan value total jumlah huruf vocal
+ */
+
+function checkVowels() {
+
+}
+
+console.log(checkVowels("I Love JavaScript")); // 6
+console.log(checkVowels("mie ayam")); //4
+
